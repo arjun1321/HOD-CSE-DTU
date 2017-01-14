5029ce156b95a3f12f4568acc32537c8c0de1bf6
arjun1321 <guptaarjun152@gmail.com>
2017-01-14 21:22:04 +0530

    international journal displayed successfully

--- includes/publications.php
+++ includes/publications.php
@@ -1,3 +1,7 @@
+<?php include "db.php"; ?>
+                  
+
+                  
 <div id="publications" class="animated fadeInRight">
                    <div class="row well">
                        <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12 col-lg-offset-1 col-md-offset-1">
@@ -19,36 +23,71 @@
                              
                            <div class="paper-list">
                                 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                                         
-                                          <div class="panel panel-info">
+                                        
+                            <?php
+                                    
+                                    $query = "SELECT * FROM papers WHERE paper_type = 'journal' ORDER BY paper_id DESC ";
+                                    $select_papers_query = mysqli_query($connection, $query);
+                                    if(!$select_papers_query) {
+                                         die("QUERY FAILED " . mysqli_error($connection));
+                                    }
+
+                                    while($row = mysqli_fetch_assoc($select_papers_query)) {
+
+                                        $paper_id = $row['paper_id'];
+                                        $paper_title = $row['paper_title'];
+                                        $paper_author = $row['paper_author'];
+                                        $paper_journal = $row['paper_journal'];
+                                        $paper_link = $row['paper_link'];
+                                        $paper_type = $row['paper_type'];
+                                        $paper_date = $row['paper_date'];
+                                        $paper_abstract = $row['paper_abstract'];
+                                        
+                            ?>
+                                              
+                                    <div class="panel panel-info">
                                             <div class="panel-heading" role="tab" id="headingOne">
-                                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne" class="accordion-link">
+                                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="<?php echo '#'.$paper_id; ?>" aria-expanded="true" aria-controls="collapseOne" class="collapsed accordion-link">
                                                  <h4 class="panel-title paper-title">
-                                                  Collapsible Group Item #1
+                                                  <?php echo $paper_title; ?>
                                                   </h4>
-                                                 <div class="paper-author"><strong>Dr.OP Verma, Dr. OP Verma</strong></div>
+                                                 <div class="paper-author"><strong><?php echo $paper_author; ?></strong></div>
                                                  
                                                  <div class="paper-conference">
-                                                     <span>Journals</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium laboriosam assumenda adipisci aliquam dolorum, tempore, rem veniam voluptatum ipsa ipsum nam ipsam expedita repudiandae, amet provident eos porro animi architecto.<span class="abstract">Abstract</span>
+                                                     <p><span>Journals</span> <span><?php echo $paper_date; ?></span> <?php echo $paper_journal; ?> 
+                                                     <p class="abstract">Abstract</span></p>
                                                  </div>
                                                  <div class="paper-link">
-                                                    <a href="#">Paper Link</a>
+                                                    <a href="<?php echo $paper_link; ?>" target="_blank">Paper Link</a>
                                                  </div>
                                                 </a>
                                               
                                             </div>
-                                            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                            <div id="<?php echo $paper_id; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
                                               <div class="panel-body">
                                                <h4 class="abstract-title">Abstract</h4>
-                                               <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+                                               <p><?php echo $paper_abstract; ?></p>
                                                
                                                 
                                               </div>
                                             </div>
-                                          </div>
+                                          </div>          
+                                               
+                            <?php
+                                        
+                                        
+                                                
+                                    }
+                                      
+                                    
+                            ?>            
+                                        
+                                        
+                                        
+                            
                                           
                                           
-                                          
+<!--
                                           <div class="panel panel-info">
                                             <div class="panel-heading" role="tab" id="headingTwo">
                                                 <a class="collapsed accordion-link" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
@@ -63,20 +102,8 @@
                                               </div>
                                             </div>
                                           </div>
-                                          <div class="panel panel-info">
-                                            <div class="panel-heading" role="tab" id="headingThree">
-                                                <a class="collapsed accordion-link" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                 <h4 class="panel-title">
-                                                  Collapsible Group Item #3
-                                                  </h4>
-                                                </a>
-                                            </div>
-                                            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                                              <div class="panel-body">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                              </div>
-                                            </div>
-                                          </div>
+-->
+                                          
                            </div>  
                              
                        </div>
