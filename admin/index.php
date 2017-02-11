06b7ef310327c1a246548eca58c46d4e3df34727
arjun1321 <guptaarjun152@gmail.com>
2017-02-11 22:58:13 +0530

    created navigation in admin panel

--- admin/index.php
+++ admin/index.php
@@ -59,19 +59,78 @@
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
-                    <li class="active">
-                        <a href="index.html">Dashboard</a>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo">Administrative Activities <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="#">View Activities</a>
+                            </li>
+                            <li>
+                                <a href="#">Add Activities</a>
+                            </li>
+                        </ul>
                     </li>
                     
                     <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="demo" class="collapse">
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo4">Professional Activities <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo4" class="collapse">
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="#">View Activities</a>
                             </li>
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="#">Add Activities</a>
                             </li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo5">Event Organised <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo5" class="collapse">
+                            <li>
+                                <a href="#">View Event</a>
+                            </li>
+                            <li>
+                                <a href="#">Add Event</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo1"> Publications <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo1" class="collapse">
+                            <li>
+                                <a href="#">View All Publications</a>
+                            </li>
+                            <li>
+                                <a href="#">Add Publications</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo2"> Events &amp; Gallery <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo2" class="collapse">
+                        
+                            <li>
+                                <a href="#">View Gallery</a>
+                            </li>
+                            <li>
+                                <a href="#">Add Gallery</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo3"> Ph.D Scholar <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo3" class="collapse">
+                            <li>
+                                <a href="#">View Ph.D Scholar</a>
+                            </li>
+                            <li>
+                                <a href="#">Add Ph.D Scholar</a>
+                            </li>
+                            
                         </ul>
                     </li>
                     
