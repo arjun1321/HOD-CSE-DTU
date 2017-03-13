a9142db5f5a71ffc5ef8fecdf5655b831ef1bf67
arjun1321 <guptaarjun152@gmail.com>
2017-03-13 13:30:21 +0530

    contact page completed

--- includes/contact.php
+++ includes/contact.php
@@ -6,12 +6,47 @@
                                 <div class="page-header">
                                     <h1>Contact</h1>
                                     <div class="hr"></div>
-                                    
+
                                 </div>
                             </div>
+                            
+                             <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-lg-offset-1 col-md-offset-1">
+                               <ul class="academic-dates-details">  
+                                    
+                                   <li style="border-left: 2px solid #03cc85; border-bottom: 2px solid #03cc85;">
+                                       <div class="scholar">
+                                           <p><span><strong>Phone: </strong></span>011-27294672</p>
+                                           <p><span><strong>Email 1: </strong></span>opverma@dce.ac.in</p>
+                                           <p><span><strong>Email 2: </strong></span>opvera.dce@gmail.com</p>
+                                       </div>
+                                   </li>
+                               </ul>
+
+                            </div>
+
+                            <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                               <ul class="academic-dates-details">
+
+                                   <li style="border-left: 2px solid #03cc85; border-bottom: 2px solid #03cc85; ">
+                                       <div class="followme">
+                                          <h3>Follow Me</h3>
+                                           <div class="social-media">
+                                                <ul>
+                                                    <li><a href="#" class="social"><i class="fa fa-facebook fa-2x"></i></a></li>
+                                                    <li><a href="#" class="social"><i class="fa fa-twitter fa-2x"></i></a></li>
+                                                    <li><a href="#" class="social"><i class="fa fa-linkedin fa-2x"></i></a></li>
+                                                </ul>
+                                            </div>
+                                       </div>
+                                   </li>
+                                   
+                                   
+                               </ul>
+
+                            </div>
+                            
                         </div>
-                        
-                        
+                                
                         
                         
                     <div class="footer">
